bcf03b50aa0e0c2b825159e0f4cddf6e95fbb9c8
JakobKSvaneborg <66070391+JakobKSvaneborg@users.noreply.github.com>
2021-06-22 11:20:38 +0200

    Update coax_waveguide.py
    updated function and fixed error

--- coax_waveguide.py
+++ coax_waveguide.py
@@ -2,8 +2,9 @@ import numpy as np
 import scipy.special as sp
 import BesselCrossZeros.calcBesselZeros as besselX
 import circular_waveguide as circWG
+import scipy.constants
 
-def coax_psiTM(a,b,m,chi,rho): #returns value of normalized TM potential in a coaxial cable of inner radius b,outer radius m. The potential is of order m and is evaluated at the point rho, where b <= rho <= a.
+def coax_phiTM(a,b,m,chi,rho): #returns value of normalized TM potential in a coaxial cable of inner radius b,outer radius m. The potential is of order m and is evaluated at the point rho, where b <= rho <= a.
 #chi is the relevant root.
     eps_m = 1 + 1*(m!=0) # 1 if m==0, otherwise 2.
     c=a/b
@@ -22,9 +23,27 @@ def coax_phiTE(a,b,m,chi,rho): #returns value of normalized TE potential in a co
     result = func*normalization
     return result
 
+def coax_phiTEM(a,b,rho):
+    return np.log(rho/b)/np.sqrt(2*np.pi*np.log(a/b))
+
+def coax_phiTEM_p(a,b,rho):
+    return 1/(rho*np.sqrt(2*np.pi*np.log(a/b)))
+
+def circ_phiTE(r,m,chi,rho):
+    eps_m = 1 + 1*(m!=0) # 1 if m==0, otherwise 2.
+    normalization=np.sqrt(eps_m/np.pi)*1/(sp.jv(m,chi)*np.sqrt(chi**2-m**2))
+    result=normalization*sp.jv(m,chi*rho/r)
+    return result
+
+def circ_phiTM(r,m,chi,rho):
+    eps_m = 1 + 1*(m!=0) # 1 if m==0, otherwise 2.
+    normalization=np.sqrt(eps_m/np.pi)*1/(sp.jv(m+1,chi)*chi)
+    result=normalization*sp.jv(m,chi*rho/r)
+    return result
+
 class waveguide_coax:
     zeros=0 #list of mode parameters
-    order=0
+    orders=0
     TE=0
     TM=0
     a=0 #outer radius
@@ -40,11 +59,26 @@ class waveguide_coax:
         self.a=a
         self.b=b
         c=a/b
-        self.zeros, self.order, self.numbers, self.TE = self.get_coaxial_modes(c,numModes,order)
+        zeros, zorder, numbers, TE = self.get_coaxial_modes(c,numModes,order)
+        TM=1-TE      
+        if order==None or order==0: #manually adds the TEM mode as the first mode, and removes the last one calculate to keep the total number of modes correct
+            self.zeros=np.concatenate((np.array([0]),zeros[:-1]))
+            self.orders=np.concatenate((np.array([0]),zorder[:-1]))
+            self.numbers=np.concatenate((np.array([0]),numbers[:-1]))
+            self.TE=np.concatenate((np.array([0]),TE[:-1]))
+            self.TM=np.concatenate((np.array([0]),TM[:-1]))
+        else:
+            self.zeros=zeros
+            self.orders=zorder
+            self.numbers=numbers
+            self.TE=TE
+            self.TM=TM
+            
+        
         self.T=self.zeros*(c-1)/(a-b) #uses standard formula to set transverse wavenumbers - this is not correct for TE(m,1) modes however. This is corrected in the next line
         TEm1_indices=(self.numbers==1)*(self.TE==1)
         self.T[TEm1_indices] = (c+1)/(a+b)*self.zeros[TEm1_indices]
-        self.TM=1-self.TE       
+        
     
 
     def get_coaxial_modes(self,q,numModes,order=None): #q is r_outer / r_inner
@@ -107,7 +141,104 @@ class waveguide_coax:
             znumber=znumbers_sorted[:numModes]
         return zeros, zorder, znumber, modetype
     
-def Coax_Circ_CouplingMatrix(WG_coax,WG_circ):
-    xi,wi=np.polynomial.legendre.leggauss(20)
+def Coax_Circ_CouplingMatrix(coax,circ):
+    a=coax.a
+    b=coax.b
+    r=circ.r
+    if(r != a):
+        print('error: radius of circular waveguide must equal outer radius of coaxial waveguide')
+    
+    L=coax.numModes
+    K=circ.numModes
+    C=np.zeros((L,K))
+    xi,wi=intba(a,b) #gauss-legendre evaluation points and weights for the appropriate integrals. the function scales these appropriately for our purpose.
+    
+    for l in range(L):
+        for k in range(K):
+            m=coax.orders[l]
+            if m != circ.order[k]:
+                continue #skip to next step if the orders of the two modes are different; there wont be any coupling between them
+            a_phi=np.pi*(1+1*(m==0)) #result of integration over phi; 2pi if m=0, otherwise pi.
+            chi_coax=coax.zeros[l]
+            chi_circ=circ.zeros[k]
+
+            if chi_coax==0: #TEM mode
+                if circ.TE[k]:
+                    #C[l,k]=circ.T[k]**2*a_phi*np.sum(wi*xi*circ_phiTE(r,m,chi_circ,xi)*coax_phiTEM(a,b,xi))
+                        C[l,k]=0
+                if circ.TM[k]:
+                    C[l,k]= - b*a_phi*circ_phiTM(r,m,chi_circ,b)*coax_phiTEM_p(a,b,b) #here there is no integration over r
+                continue #goes to next loop iteration if the coax mode was TEM
+
+            #in this part of the loop, the coaxial mode cannot be a TEM mode
+
+            if coax.TE[l] and circ.TE[k]: #TE-TE case
+                C[l,k]=a_phi*coax.T[l]**2*np.sum(wi*xi*circ_phiTE(r,m,chi_circ,xi)*
+                                                coax_phiTE(a,b,m,chi_coax,xi))
+                continue
+
+            if coax.TM[l] and circ.TM[k]: #TM-TM case
+                C[l,k]=a_phi*circ.T[k]**2*np.sum(wi*xi*circ_phiTM(r,m,chi_circ,xi)*
+                                                coax_phiTM(a,b,m,chi_coax,xi))
+                continue
+
+            if coax.TM[l] and circ.TE[k]:
+                C[l,k]=0
+                continue
+
+            if coax.TE[l] and circ.TM[k]:
+                #this formula is implemented under the assumption that the coaxial mode has cosine polarization. the induced circular mode will then have sine polarization. I think the sign changes if the two polarizations were switched.
+                C[l,k]= b*(m)*a_phi * coax_phiTE(a,b,m,chi_coax,b)*circ_phiTM(r,m,chi_circ,b)
+
+            #that should be all
+                
+    return C
+        
+        
+def intba(a,b,N=20): #gauss-legendre weights and points for integration between x=b and x=a
+    xi,wi=np.polynomial.legendre.leggauss(N)  #for numerical integration: temp.
+    yi=(xi+1)/2 #shifts the xi from the interval [-1,1] to [0,1]
+    xi=b+yi*(a-b) #these are the evaluation point for integration
+    wi=wi*(a-b)/2 # new weights for the new integration range
+    return xi,wi
+    
+
+def computeSMatrix(coax,circ,frequency,C12=0): #frequency in Hz. C12 is passed as argument to avoid repeat calculations
+    k=2*np.pi*frequency/299792458 # free space wavenumber
+    #Impedance and admittance matrices
+    eps0=scipy.constants.epsilon_0
+    mu0=scipy.constants.mu_0
+    Z0=np.sqrt(mu0/eps0) #normalization
+    if np.all(C12==0):
+        C12=Coax_Circ_CouplingMatrix(coax,circ)
     
     
+    #formulas: ZTE/Z0 = jk0 / gamma ; ZTM/Z0 = gamma / jk0
+    
+    #propagation constants
+    gamma1=np.sqrt(coax.T**2-k**2,dtype=complex)
+    gamma2=np.sqrt(circ.T**2-k**2,dtype=complex)
+    #gamma2=np.sqrt(wg1.T**2-k**2,dtype=complex) #THESE ARE NOW SWITCHED
+    #gamma1=np.sqrt(wg2.T**2-k**2,dtype=complex) #THESE ARE NOW SWITCHED
+    Z1=Z0*(coax.TE*1j*k/gamma1 + coax.TM*gamma1/(1j*k))
+    Z2=Z0*(circ.TE*1j*k/gamma2 + circ.TM*gamma2/(1j*k))
+    Z1[(coax.TE==0)*(coax.TM==0)]=Z0 #set impedance of TEM mode
+    Y1=np.diag(1/Z1)
+    Y2=np.diag(1/Z2) #not actually necessary
+    Z1=np.diag(Z1)
+    Z2=np.diag(Z2)
+    C21=C12.T    
+    #my derivation - in the circular case, this works only when r1 < r2
+    A=np.concatenate((np.concatenate((-C21@np.sqrt(Z1),np.sqrt(Z2)),axis=1),np.concatenate((np.sqrt(Y1),C12@np.sqrt(Y2)),axis=1)),axis=0)
+    B=np.concatenate((np.concatenate((C21@np.sqrt(Z1),-np.sqrt(Z2)),axis=1),np.concatenate((np.sqrt(Y1),C12@np.sqrt(Y2)),axis=1)),axis=0)
+    
+    
+    #Michael's derivation - one step back from final matrix - this works only when r1>r2
+    #A=np.concatenate((np.concatenate((np.sqrt(Z1),-C12@np.sqrt(Z2)),axis=1),np.concatenate((C21@np.sqrt(Y1),np.sqrt(Y2)),axis=1)),axis=0)
+    #B=np.concatenate((np.concatenate((-np.sqrt(Z1),C12@np.sqrt(Z2)),axis=1),np.concatenate((C21@np.sqrt(Y1),np.sqrt(Y2)),axis=1)),axis=0)
+    
+    S=np.linalg.inv(B)@A
+    return S
+    
+def dB(S):
+    return 20*np.log10(np.abs(S))
